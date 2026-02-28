0191da4a0a990386d1258576087ae56f2f12fa8f
Yash-Kumar-Vaishnav <yashkumarvaishnav19@gmail.com>
2026-02-28 11:06:34 +0530

    Folder upload

---
+++ 27-02-26/WebApplication2/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication2.Models
+{
+    public class Product
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+
+    }
+}
---
+++ 27-02-26/WebApplication3/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+using System.Collections.Generic;
+
+namespace WebApplication2.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Pen", Description = "Blue Ink Pen", Price = 10.0 },
+            new Product { Id = 2, Name = "Pencil", Description = "HB Pencil", Price = 20.0 },
+            new Product { Id = 3, Name = "Rubber", Description = "Eraser", Price = 30.0 }
+        };
+
+        public IActionResult Index()
+        {
+            return View(products);
+        }
+    }
+}
